60019d4190e5fc58e8d5edffe60ee1857cf4c16b
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-04 22:38:36 +0000

    Added Bootstrap to our files

--- run.py
+++ run.py
@@ -14,6 +14,11 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+    
+@app.route('/careers')
+def careers():
+    return render_template('careers.html')
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
